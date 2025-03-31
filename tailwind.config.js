18c3b47d20cc4d000060b79b6dffd2354707653c
addbankai <rtflaviano25@gmail.com>
2025-03-31 13:44:43 +0800

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)'], // Set Poppins as default sans-serif
+        orbitron: ['var(--font-orbitron)'], // Add Orbitron
+      },
+      // Add any other theme extensions here if needed
+    },
+  },
+  plugins: [],
+};
